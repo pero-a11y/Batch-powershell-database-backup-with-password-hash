321cb1fceafaa5156a6971c1bd88ea655e732bee
pero-a11y <laruam.pero@gmail.com>
2026-02-09 03:41:34 +0100

    Delete sifriranje.ps1

--- sifriranje.ps1
+++
@@ -1,3 +0,0 @@
-$geslo = "!Maribor2024"
-$sifrirajgeslo = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($geslo))
-Set-Content -Path "pass.txt" -Value "DB_PASSWORD=$sifrirajgeslo" -Force
